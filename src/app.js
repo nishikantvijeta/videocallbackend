22532dc65d86391a350e7b68dbd1f5ee1db2c315
nishikantvijeta <128150871+nishikantvijeta@users.noreply.github.com>
2026-05-22 21:13:01 +0530

    Add dotenv configuration for environment variables

--- src/app.js
+++ src/app.js
@@ -13,7 +13,9 @@ const app = express();
 const server = createServer(app);
 const io = connectToSocket(server);
 
+import dotenv from "dotenv";
 
+dotenv.config();
 app.set("port", (process.env.PORT || 8000))
 app.use(cors());
 app.use(express.json({ limit: "40kb" }));
